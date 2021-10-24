b9e9d4784c4a49c11aacb78afb33ee38e6650479
Konstantin <Steelmen87@bk.ru>
2021-10-24 22:54:43 +0300

    Next 86

--- src/components/Profile/MyPosts/ProfileInfo/ProfileStatusFunction.tsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileStatusFunction.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 
 type PropsType = {
@@ -13,6 +13,9 @@ type StateType = {
 const ProfileStatusWithHooks = (props) => {
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+    useEffect(()=>{
+        setStatus(props.status)
+    },[props.status])
     const activateEditMode = () => {
         setEditMode(true)
     }
