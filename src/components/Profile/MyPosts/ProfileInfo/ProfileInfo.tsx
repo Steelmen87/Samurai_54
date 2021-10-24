b82424023a4eeffd346d41b55624517431eff9ff
Konstantin <Steelmen87@bk.ru>
2021-10-24 22:21:41 +0300

    Next 85

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from './ProfileInfo.module.css'
 import {Preloader} from "../../../common/preloader/Preloader";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusFunction";
 
 
 const ProfileInfo = (props) => {
@@ -18,7 +19,7 @@ const ProfileInfo = (props) => {
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} alt=''/>
 
-                <ProfileStatus status={props.status}
+                <ProfileStatusWithHooks status={props.status}
                                updateStatus={props.updateStatus}/>
             </div>
         </div>
